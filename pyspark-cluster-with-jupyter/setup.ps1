587e909335eed5cb759f36895c1eca7f716e3f0a
brijrajk <22271048+brijrajk@users.noreply.github.com>
2024-09-08 01:23:53 +0530

    Updated setup.sh

---
+++ pyspark-cluster-with-jupyter/setup.ps1
@@ -0,0 +1,53 @@
+# Define your GitHub repository URL and directories
+$repoUrl = "https://github.com/brijrajk/docker-spark.git"
+$repoDir = "docker-spark"
+
+# Function to handle errors
+function ExitWithError {
+    param (
+        [string]$message
+    )
+    Write-Error $message
+    exit 1
+}
+
+# Clone or pull the repository
+if (Test-Path $repoDir) {
+    Write-Output "Repository already exists. Pulling the latest changes..."
+    Set-Location $repoDir
+    try {
+        git pull
+    } catch {
+        ExitWithError "Failed to pull the repository. Please check your Git setup."
+    }
+} else {
+    Write-Output "Cloning the repository from $repoUrl..."
+    try {
+        git clone $repoUrl
+    } catch {
+        ExitWithError "Failed to clone the repository. Please check your Git setup."
+    }
+    Set-Location $repoDir
+}
+
+# Build Docker images
+Write-Output "Building Docker images..."
+try {
+    ./build.ps1
+} catch {
+    ExitWithError "Failed to build Docker images. Please check your build script."
+}
+
+# Navigate back to the parent directory
+Set-Location ..
+
+# Run docker-compose
+Write-Output "Starting services with Docker Compose..."
+try {
+    docker-compose up --build
+} catch {
+    ExitWithError "Failed to start services with Docker Compose. Please check your Docker setup."
+}
+
+Write-Output "Setup complete. Spark cluster is up and running!"
+
---
+++ pyspark-cluster-with-jupyter/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+# Define your GitHub repository URL and directories
+REPO_URL="https://github.com/brijrajk/docker-spark.git"
+REPO_DIR="docker-spark"
+
+# Clone or pull the repository
+if [ -d "$REPO_DIR" ]; then
+    echo "Repository already exists. Pulling the latest changes..."
+    cd $REPO_DIR
+    git pull
+else
+    echo "Cloning the repository from $REPO_URL..."
+    git clone $REPO_URL
+    cd $REPO_DIR
+fi
+
+
+
+# Build Docker images
+echo "Building Docker images..."
+./build.sh
+
+
+cd ..
+
+# Run docker-compose
+echo "Starting services with Docker Compose..."
+docker-compose up --build
+
+echo "Setup complete. Spark cluster is up and running!"
+
